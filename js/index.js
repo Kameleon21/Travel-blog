95a72c6a5bc74426dcb9c60ac99779f4cffe6b02
Kameleon21 <kamilrogozinski29@gmail.com>
2023-04-15 19:50:01 +0100

    Added animation and updaed main section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
+// animations for main page main elements
+$(() => {
+  let offset = { offset: "60%" }; // when items appear
+  $(".main-head").waypoint(function () {
+    $(".main-head").addClass("animate__animated animate__fadeInRight");
+  }, offset);
 
+  $(".num-one").waypoint(function () {
+    $(".num-one").addClass("animate__animated animate__fadeInLeft");
+  }, offset);
+
+  $(".num-two").waypoint(function () {
+    $(".num-two").addClass("animate__animated animate__fadeInRight");
+  }, offset);
+
+  $(".num-three").waypoint(function () {
+    $(".num-three").addClass("animate__animated animate__fadeInLeft");
+  }, offset);
+
+  $(".num-four").waypoint(function () {
+    $(".num-four").addClass("animate__animated animate__fadeInRight");
+  }, offset);
+});
